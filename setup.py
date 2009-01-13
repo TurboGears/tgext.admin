2edbb0263c600a90a152ab524e22cfd7fd57820a
percious17 <devnull@localhost>
2009-01-13 05:39:23 +0000

    added shell for tgext.admin

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='tgext.admin',
+      version=version,
+      description="Admin Controller add-on for basic TG identity model.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='TG2, TG, sprox, Rest, internet, adminn',
+      author='Christopher Perkins',
+      author_email='chris@percious.com',
+      url='tgtools.googlecode.com',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['tgext'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
