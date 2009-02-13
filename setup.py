e3842ef1f4eb7043b2eaa6c99c95045271931688
percious17 <devnull@localhost>
2009-02-13 20:17:38 +0000

    tagging 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.2'
 
 setup(name='tgext.admin',
       version=version,
@@ -24,6 +24,8 @@ setup(name='tgext.admin',
       zip_safe=True,
       install_requires=[
           'setuptools',
+          'sprox',
+          'tgext.crud',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
