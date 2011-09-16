1d5735c111f4f5a0623b693d1531c3e7a0845a08
Alessandro Molina <alessandro.molina@axant.it>
2011-09-16 09:36:51 +0200

    Increase to version 0.3.11 due to TG-dev requiring it for ming support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3.10'
+version = '0.3.11'
 here = os.path.abspath(os.path.dirname(__file__))
 try:
     README = open(os.path.join(here, 'README.txt')).read()
