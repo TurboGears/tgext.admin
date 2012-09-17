8b8f721440aa18f7199e4b082b7360dc62ba0165
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@blackchairsoft.com>
2012-09-17 12:39:01 -0700

    Added Jinja as default templating system support

--- tgext/admin/controller.py
+++ tgext/admin/controller.py
@@ -47,8 +47,10 @@ class AdminController(TGController):
                     default_renderer = 'genshi'
                 elif 'mako' in tg_config.renderers:
                     default_renderer = 'mako'
+                elif 'jinja' in tg_config.renderers:
+                    default_renderer = 'jinja'
                 else:
-                    log.warn('TurboGears admin supports only Genshi ad Mako, please make sure you add at \
+                    log.warn('TurboGears admin supports only Genshi, Mako and Jinja, please make sure you add at \
     least one of those to your config/app_cfg.py base_config.renderers list.')
                     self.missing_template = True
 
@@ -61,7 +63,7 @@ class AdminController(TGController):
     @expose('tgext.admin.templates.index')
     def index(self):
         if self.missing_template:
-            raise Exception('TurboGears admin supports only Genshi ad Mako, please make sure you add at \
+            raise Exception('TurboGears admin supports only Genshi, Mako and Jinja, please make sure you add at \
     least one of those to your config/app_cfg.py base_config.renderers list.')
 
         #overrides the template for this method
